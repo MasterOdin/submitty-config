77f426a989ad5e442da5b3cdc818d92c6bd77d16
Matthew Peveler <matt.peveler@gmail.com>
2018-05-05 21:51:03 -0400

    0.5.0 release

--- submitty_config/__init__.py
+++ submitty_config/__init__.py
@@ -3,7 +3,7 @@ __DESCRIPTION__ = 'My short description for my project.'
 __EMAIL__ = 'matt.peveler@gmail.com'
 __AUTHOR__ = 'Matthew Peveler'
 __LICENSE__ = 'MIT'
-__VERSION__ = '0.4.0'
+__VERSION__ = '0.5.0'
 __URL__ = 'https://github.com/Submitty/submitty-config'
 
 
